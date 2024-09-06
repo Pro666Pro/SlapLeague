81df6f0a18d91b5b57b7fb86d3b4cebd73f2b390
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-09-06 20:05:15 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-local GameName = "Nexer Hub - Slap League 👏"
+local GameName = "Slap League 👏"
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({IntroText = "Nexer Hub 👏", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
+local Window = OrionLib:MakeWindow({IntroText = "Slap League 👏", IntroIcon = "rbxassetid://15315284749",Name = GameName, HidePremium = false, SaveConfig = true, ConfigFolder = "Tutorial"})
 
 OrionLib:MakeNotification({Name = "Warning",Content = "Use at your own risk.",Image = "rbxassetid://7733658504",Time = 5})
 
